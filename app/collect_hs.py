8f5f32749573b9f03df2c0bac8ce5ad3f25beb14
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2016-03-06 14:25:32 +0200

    Add data collection and processing pipeline

---
+++ app/collect_hs.py
@@ -0,0 +1,101 @@
+import requests
+import bs4
+import json
+import re
+
+def _comment( story , index = 0):
+
+    path = "http://www.hs.fi/ajax/keskustelut/" + story + "?startIndex=" + str(index) + "&order=a"
+
+    r = requests.get( path )
+    text = r.text.strip()
+
+    ## no more comments left
+    if text == '':
+        return False
+
+    html = bs4.BeautifulSoup( text )
+
+    ret = []
+
+    comments = html.find_all('div', class_='comment-text')
+    comments_meta = html.find_all('div', class_= 'meta')
+
+    for i, c in enumerate( comments ):
+        comment = {}
+
+        comment['story'] = story
+
+        meta = comments_meta[ i ]
+
+        ## comment id
+        comment['id'] = meta['id'].replace('comment-meta', '')
+
+        ## author name
+        comment['author'] = meta.find('a' , class_='author' ).text
+
+        ## samaa mielta score
+        scores = c.find_all('div', class_='comment-btn-container')[0]
+        scores = scores.find_all('span', class_='rating-value')
+        comment['agreement_yes'] = int( scores[0].text[1:-1] )
+        comment['agreement_no'] = int( scores[1].text[1:-1] )
+
+        ## hyvin argumentoitu score
+        scores = c.find_all('div', class_='comment-btn-container')[1]
+        scores = scores.find_all('span', class_='rating-value')
+        comment['argument_yes'] = int( scores[0].text[1:-1] )
+        comment['argument_no'] = int( scores[1].text[1:-1] )
+
+        ## comment text
+        c.find( class_ = 'comment-text-links').decompose() ## remove non-comment stuff
+        comments = c.find_all('p')
+        comment['text'] = '\n'.join( map( lambda x: x.text , comments ) )
+
+        ret.append( comment )
+
+    return ret
+
+def comment( story ):
+
+    comments = []
+
+    current = _comment( story )
+
+    while current:
+
+        comments += current
+
+        current = _comment( story , len( comments ) + 1 )
+
+    return comments
+
+## http://www.hs.fi/ajax/keskustelut/1296808743968/a1305922917503?startIndex=3&order=a
+## http://www.hs.fi/ajax/keskustelut/1296808474403/a1305922917503?startIndex=0&order=a
+
+def collect_for_stories():
+
+    collected = []
+
+    data = []
+
+    r = requests.get( 'http://www.hs.fi/uutiset/?ref=hs-navi-osastot' )
+    html = bs4.BeautifulSoup( r.text )
+
+    for link in html.find_all( 'a' , class_='article-link'):
+
+        path1 = '1296808743968' ## link['data-department-id']
+        path2 = link['data-article-id']
+
+        if path2 not in collected:
+
+            data += comment( path1 + '/a' + path2 )
+
+            collected.append( path2 )
+
+    json.dump( data, open('data-20150209.json', 'w') )
+
+
+
+if __name__ == '__main__':
+
+    collect_for_stories()
--- app/main.py
+++ app/main.py
@@ -1,14 +1,77 @@
 from flask import *
 
+import collect_hs
+
+import collections
+
+import nltk
+
+import numpy
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+
 app = Flask(__name__)
+
+
+## common constructs
+stem = nltk.stem.snowball.SnowballStemmer('finnish')
 
 @app.route("/")
 def index():
     return send_from_directory( 'static' , 'index.html' )
 
-@app.route('/api/topicmodel', methods=['POST'] )
+@app.route('/topicmodel', methods=['POST'] )
 def analyze():
-    return None
+
+    path1 = request.form['url'].split('/')[-1]
+    path = '1296808743968/' + path1
+
+    comments = collect_hs.comment( path )
+
+    _texts = []
+
+    for c in comments:
+        text = nltk.word_tokenize( c['text'] )
+        text = map( lambda x: stem.stem( x ) , text )
+        _texts.append( ' '.join( text ) )
+
+
+    tf_vectorizer = CountVectorizer(
+                max_df=0.95,
+                min_df=2,
+                max_features= 10000 )
+    texts = tf_vectorizer.fit_transform( _texts )
+
+    ## test between 2 and 20 topics
+    topics = {}
+
+    for k in range(2, 21):
+        model = LatentDirichletAllocation(
+                    n_topics= k ,
+                    max_iter=5,
+                    learning_method='online',
+                    learning_offset=50.,
+                    random_state=0
+                )
+        fit = model.fit( texts )
+        ll = model.score( texts )
+        topics[ ll ] = fit
+
+    topic = max( topics.keys() )
+
+    ret = collections.defaultdict( list )
+    ## ugly, rewrite some day
+    new_topics = topics[ topic ].transform( texts )
+    for i, topic in enumerate( new_topics ):
+
+        topic = numpy.argmax( topic )
+        text = _texts[ i ].encode('utf8')
+
+        print text
+
+        ret[ topic ].append( text )
+
+    return jsonify( ret )
 
 if __name__ == "__main__":
-    app.run()
+    app.run( debug = True)
