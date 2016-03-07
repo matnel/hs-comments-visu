8d69bff877843b21791e822ff010e53af6f4e3ba
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2016-03-07 17:35:53 +0200

    Updates in ML algorithm

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 
 ## common constructs
 stem = nltk.stem.snowball.SnowballStemmer('finnish')
+stopwords = map( lambda x: x.strip().decode('utf8'), open('stopword.txt').readlines() )
 
 def topicmodel( comments ):
 
@@ -23,40 +24,51 @@ def topicmodel( comments ):
 
     for c in comments:
 
-        _texts.append( c['text'] )
+        c = c['text']
+        _texts.append( c )
+        texts.append( c )
 
-        text = nltk.word_tokenize( c['text'] )
-        text = map( lambda x: stem.stem( x ) , text )
-        texts.append( ' '.join( text ) )
+
 
     tf_vectorizer = CountVectorizer(
-                max_df=.95,
-                min_df=2,
-                max_features= 10000 )
+                max_df=.20,
+                min_df=10,
+                stop_words = stopwords )
     texts = tf_vectorizer.fit_transform( texts )
 
-    ## test between 2 and 50 topics
+    ## test between 2 and 20 topics
     topics = {}
 
-    for k in range(2, 51):
+    for k in range(2, 10):
+
+        print "Testing", k
+
         model = LatentDirichletAllocation(
                     n_topics= k ,
                     max_iter=5,
-                    learning_method='online',
+                    learning_method='batch',
                     learning_offset=50.,
                     random_state=0
                 )
-        fit = model.fit( texts )
+        model.fit( texts )
         ll = model.score( texts )
-        topics[ ll ] = fit
+        topics[ ll ] = model
 
     topic = max( topics.keys() )
 
     ret = collections.defaultdict( list )
 
     ## ugly, rewrite some day
-    new_topics = topics[ topic ].transform( texts )
-    for i, topic in enumerate( new_topics ):
+    model = topics[ topic ]
+
+    ## for debug pront chosen models' names
+    feature_names = tf_vectorizer.get_feature_names()
+    for topic_idx, topic in enumerate(model.components_):
+        print "Topic #%d:" % topic_idx
+        print " ".join( [feature_names[i].encode('utf8') for i in topic.argsort()[:-5 - 1:-1]])
+        print
+
+    for i, topic in enumerate( model.transform( texts ) ):
 
         topic = numpy.argmax( topic )
         text = _texts[ i ].encode('utf8')
